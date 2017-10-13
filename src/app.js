4e65209597c7a154dcd816f2a8e2954fcccd98a9
marcpre <marcus.presich@gmail.com>
2017-10-13 16:46:14 +0000

    fixing views

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-require("dotenv").config()
 const express = require("express")
 const path = require("path")
 const logger = require("morgan")
@@ -6,26 +5,26 @@ const bodyParser = require("body-parser")
 const app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "/../views"))
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "pug")
 
-app.use(logger(process.env.LOG_ENV))
+app.use(logger("debug"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false, }))
-app.use(express.static(path.join(__dirname, "public"))) //public folder!
+app.use(express.static(path.join(__dirname, "/../public"))) //public folder!
 
 //routes
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req, res) {
+    res.render("index", {
+    })
 })
-
 
 //Start Server
 const port = process.env.APP_PORT || 8080
 const host = process.env.APP_HOST || "localhost"
 
 app.listen(port, function() {
-  console.log("Listening on " + host + ":" + port)
+    console.log("Listening on " + host + ":" + port)
 })
 
 module.exports = app
