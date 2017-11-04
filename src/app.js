e8e65b91996dfd29f91bc110f353d870c6aea62e
marcpre <marcus.presich@gmail.com>
2017-11-04 13:53:17 +0100

    fix eslint errors

--- src/app.js
+++ src/app.js
@@ -1,34 +1,35 @@
 require('dotenv').config()
-const express = require("express")
-const fs = require("fs")
-const path = require("path")
-const logger = require("morgan")
-const bodyParser = require("body-parser")
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
 const app = express()
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "pug")
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 app.use(logger(process.env.LOG_ENV))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended: false,
+  extended: false,
 }))
-app.use(express.static(path.join(__dirname, "/../public"))) //public folder!
+app.use(express.static(path.join(__dirname, '/../public'))) // public folder!
 
-//routes
-app.get('/', function (req, res) {
-    const data = JSON.parse(fs.readFileSync(path.join(__dirname, "./data/bitcoinPrice.json"), 'utf8'))
-    res.render("index")
+// routes
+app.get('/', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(path.join(__dirname, './data/bitcoinPrice.json'), 'utf8'))
+  res.render('index')
 })
 
-//Start Server
+// Start Server
 const port = process.env.APP_PORT || 8080
-const host = process.env.APP_HOST || "localhost"
+const host = process.env.APP_HOST || 'localhost'
 
-app.listen(port, function () {
-    console.log("Listening on " + host + ":" + port)
+app.listen(port, () => {
+  console.log(`Listening on ${host}:${port}`)
 })
 
 module.exports = app
